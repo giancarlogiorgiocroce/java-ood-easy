ec4ba1628c0eb081aade805bbfb61f44772c8029
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-11-04 16:14:02 +0100

    Iniziata a popolare un Utente

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 class HelloWorld {
     public static void main(String[] args) {
-        
+        User youngUser = new User();
+
+        youngUser.name = "G. G. Croce";
+        youngUser.birthday = LocalDate.parse("1995-04-18");
+
+        System.out.printf("Il tuo nome: %s \nLa tua data di nascina: %s \nI tuoi anni: %d \n", youngUser.name, youngUser.birthday, youngUser.age());
     }
 }
--- User.java
+++ User.java
@@ -1,6 +1,12 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class User {
     public String name;
     public LocalDate birthday;
+
+    public int age() {
+        int age = Period.between(this.birthday, LocalDate.now()).getYears();
+        return age;
+    }
 }
